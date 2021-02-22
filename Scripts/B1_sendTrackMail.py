9f60c15bbf8e1d9922e4eb01644443f85f30a471
idan054 <idanbit80@gmai.com>
2021-02-22 19:56:59 +0200

    V1.3 Wordpress - Butik 24
    (guiDesign.py as main.py)
    Updated .exe on dist (2)
    
    V1.3 Close order by changing to "Complete" when send_track_mail()
    
    V1.2 fix street field & other improvements
    
    V1.1 "Continue" button just works.
         "Send tracking mail" button just works.

--- Scripts/B1_sendTrackMail.py
+++ Scripts/B1_sendTrackMail.py
@@ -1,4 +1,7 @@
 import winsound
+from time import sleep
+
+from selenium.webdriver.common.keys import Keys
 
 from Scripts.A2_goToTab import goToTab
 
@@ -33,6 +36,23 @@ def send_track_mail(browser, finalOrderLink,
     sendMailButton = browser.find_element_by_xpath(
         '//*[@id="woocommerce-order-notes"]/div[2]/div/p[2]/button')
     sendMailButton.click()
+
+    sleep(0.5)
+
+    ## Change order status to "Complete"
+    # Scroll to top
+    browser.execute_script("scroll(0, 0);")
+    status_dropdown_button = browser.find_element_by_xpath('//*[@id="select2-order_status-container"]')
+    status_dropdown_button.click()
+    status_dropdown_search_field = browser.find_element_by_xpath('/html/body/span/span/span[1]/input')
+    status_dropdown_search_field.send_keys("סופק")
+    status_dropdown_search_field.send_keys(Keys.ENTER)
+    # Class: select2-order_status-result-emd1-wc-completed
+    # Xpath: //*[@id="select2-order_status-result-emd1-wc-completed"]
+    # supplied_mode = browser.find_element_by_class('select2-order_status-result-emd1-wc-completed')
+    # supplied_mode.click()
+    save_button = browser.find_element_by_name('save')
+    save_button.click()
 
     winsound.Beep(2000, 150)
     winsound.Beep(1500, 150)
